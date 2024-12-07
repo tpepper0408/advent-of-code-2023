64fcec0ced5c610f7dd56ba1d16b2a44f95ba8bb
Tim Pepper <tim.pepper04@gmail.com>
2024-12-07 15:41:08 +0000

    2024 Day 8 - Skeleton

---
+++ solutions/aockt/y2024/Y2024D08.kt
@@ -0,0 +1,13 @@
+package aockt.y2024
+
+import io.github.jadarma.aockt.core.Solution
+
+class Y2024D08 : Solution {
+    override fun partOne(input: String): Any {
+        return -1
+    }
+
+    override fun partTwo(input: String): Any {
+        return -1
+    }
+}
---
+++ tests/aockt/y2024/Y2024D08Test.kt
@@ -0,0 +1,14 @@
+package aockt.y2024
+
+import io.github.jadarma.aockt.test.AdventDay
+import io.github.jadarma.aockt.test.AdventSpec
+
+@AdventDay(2024, 8, "Day 8: Title")
+class Y2024D08Test : AdventSpec<Y2024D08>({
+    partOne {
+        "input" shouldOutput 0
+    }
+    partTwo {
+        "input" shouldOutput 0
+    }
+})
